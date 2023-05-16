aa876aea0554f0e1d46660fba7f2901df0c6fa5a
faizrahimiroslan97 <faizrahimiroslan97@gmail.com>
2023-05-16 16:49:07 +0800

    Tailwind & CSS Update
    -Added global CSS for general use.
    -Added extensions for Tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,24 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+        robotocondensed: ["Roboto Condensed", "sans-serif"],
+        robotoslab: ["Roboto Slab", "sans-serif"],
+      },
+      height: {
+        "176": "44rem;",
+      },
+      width: {
+        "128": "32rem;",
+        "176": "44rem;",
+      },
+      backgroundImage: {
+        login: "url('img/forest-bg.jpg')",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
